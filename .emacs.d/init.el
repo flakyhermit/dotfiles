46419f049f1e4ba711d5e5469a7ed6701e6cacea
Jewel James <flakyhermit@protonmail.ch>
2020-05-09 11:39:42 +0530

    org-mode-edits

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -13,13 +13,14 @@
 (tool-bar-mode 0)
 (menu-bar-mode 0)
 (scroll-bar-mode 0)
+(global-visual-line-mode t)
 ;; load
 (load-theme 'nord t)
 ;; Disable the ugly Emacs bull and the info
 (setq inhibit-startup-message t)
 ;; set global face settings
-(set-face-attribute 'variable-pitch nil :family "IBM Plex Sans" :height 105 :weight 'medium)
-(set-face-attribute 'fixed-pitch nil :family "JetBrains Mono" :height 90 :weight 'semi-bold)
+(set-face-attribute 'variable-pitch nil :family "IBM Plex Sans" :height 1.25 :weight 'regular)
+(set-face-attribute 'fixed-pitch nil :family "JetBrains Mono" :height 85 :weight 'semi-bold)
 (set-face-attribute 'default nil :family "JetBrains Mono" :height 90 :weight 'semi-bold)
 ;; Global keybindings
 (global-set-key (kbd "C-x C-t") 'eshell)
@@ -46,14 +47,14 @@
 (use-package org
   :config
   (set-face-attribute 'org-document-title nil :family "IBM Plex Serif" :height 140 :weight 'semi-bold)
-  (set-face-attribute 'org-level-1 nil :family "IBM Plex Serif" :height 120 :weight 'semi-bold))
+  (set-face-attribute 'org-level-1 nil :family "IBM Plex Serif" :height 120 :weight 'semi-bold)
+  (set-face-attribute 'org-verbatim nil :family "JetBrains Mono" :height 90 :weight 'semi-bold)
+  (set-face-attribute 'org-code nil :family "JetBrains Mono" :height 90 :weight 'semi-bold))
 
 (add-hook 'org-mode-hook
 	  (lambda ()
-	    (setq left-margin-width 2)))
-
-(use-package org-superstar
-  :init (add-hook 'org-mode-hook #'org-superstar-mode))
+	    (setq left-margin-width 2)
+	    (setq right-margin-width 2)))
 (use-package org-indent
   :init (add-hook 'org-mode-hook #'org-indent-mode))
 (use-package mixed-pitch
@@ -89,11 +90,6 @@
   :config (setq deft-directory "~/Documents/org"
                 deft-extensions '("org")
 		deft-default-extension "org")
-          (set-face-attribute 'deft-title-face nil :family "JetBrains Mono" :height 105 :weight 'bold)
-	  (set-face-attribute 'deft-summary-face nil :family "JetBrains Mono" :height 90 :weight 'regular)
-	  (set-face-attribute 'deft-header-face nil :family "JetBrains Mono" :height 105 :weight 'bold)
-    
-	  
   :init (add-hook 'deft-mode-hook #'xaf-deft-mode-settings))
 
 ;; olivetti
@@ -106,3 +102,22 @@
   :init (add-hook 'olivetti-mode-hook #'xaf-olivetti-mode-setup))
 
 (evil-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("b11699e28cc2f6c34fa6336e67d443be89fadb6a9b60de0b1594f31340ea87e4" "6973f93f55e4a6ef99aa34e10cd476bc59e2f0c192b46ec00032fe5771afd9ad" default)))
+ '(ivy-mode t)
+ '(org-roam-directory "~/Documents/org")
+ '(package-selected-packages
+   (quote
+    (markdown-mode use-package sexy-monochrome-theme projectile org-superstar olivetti nord-theme mixed-pitch magit ivy evil-snipe elpy doom-themes deft company-org-roam color-theme-sanityinc-tomorrow busybee-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
