c204639a8e1e074700417df6f3446b42c81dc995
Jewel James <jeweljames@protonmail.com>
2020-06-26 10:01:38 +0530

    Major changes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,18 +1,18 @@
-;; Added by Package.el.  This must come before configurations of
+
+;; ADDED by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 ;; Add MELPA repo
 (require 'package)
-;; (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
-(require 'use-package)
-;; (require 'use-package-ensure)
-;; (setq use-package-always-ensure t)
 ;; Set custom theme directory
 (setq custom-theme-directory "~/.emacs.d/themes")
 (setq backup-directory-alist `(("." . "~/.saves")))
+;; Set default frame size
+(add-to-list 'default-frame-alist '(height . 36))
+(add-to-list 'default-frame-alist '(width . 105))
 ;; Disable all GUI crap
 (tool-bar-mode 0)
 (menu-bar-mode 0)
@@ -22,8 +22,9 @@
 (column-number-mode t)
 ;; set theme
 (load-theme 'doom-acario-light t)
+;; (load-theme 'zeno t)
 ;; Disable the ugly Emacs bull and the info
-;; (setq inhibit-startup-message t)
+(setq inhibit-startup-message t)
 ;; set custom face settings
 (set-face-attribute 'variable-pitch nil :family "Dejavu Sans Mono" :height 115 :weight 'regular)
 (set-face-attribute 'fixed-pitch nil :family "IBM Plex Mono" :height 90 :weight 'regular)
@@ -37,7 +38,6 @@
 (global-set-key (kbd "C-x k") 'kill-this-buffer)
 
 ;; Enable minor modes
-;; (ivy-mode)
 ;; Enable line numbers by default
 ;; (global-display-line-numbers-mode)
 
@@ -48,36 +48,86 @@
 (add-hook 'text-mode-hook
 	  (lambda ()
 	    (display-line-numbers-mode -1)
-;;	    (flyspell-mode)
+;;   (flyspell-mode)
 ;;	    (variable-pitch-mode 'toggle)
 	    ))
 
 (add-hook 'dired-mode-hook
 	  (lambda ()
+	    (dired-hide-details-mode)
 	    (define-key dired-mode-map "-" 'dired-up-directory)))
-;; Package configurationsx
+
+;; Custom extensions
+(add-to-list 'load-path "~/.emacs.d/custom")
+(require 'personal-journal)
+
+;; Package configurations
+;; Additional packages to install
+;; evil, evil-snipe, crux, ivy, magit, projectile,
+;; org-roam, markdown-mode, org-ref, org-roam-bibtex
+;; deft, olivetti, delight
 (require 'use-package)
+
+;; recentf ------------------------
+(use-package recentf
+  :custom
+  (recentf-max-saved-items 100)
+  :init
+  (recentf-mode 1))
 
 ;; crux ---------------------------
 (use-package crux
-  :bind ("C-x C-r" . crux-recentf-find-file)
-        ("C-x C-K" . crux-kill-other-buffers))
+  :bind
+  ("C-x C-r" . crux-recentf-find-file)
+  ("C-x _" . crux-delete-file-and-buffer)
+  ("C-x C-K" . crux-kill-other-buffers))
 
 ;; ivy ----------------------------
-(use-package ivy)
+(use-package ivy
+  :config
+  (ivy-mode 1))
+
+;; amx ----------------------------
+(use-package amx
+  :ensure t
+  :after ivy
+  :custom
+  (amx-backend 'auto)
+  :config
+  (amx-mode 1))
+
+;; counsel -------------------------
+(use-package counsel
+  :ensure t
+  :after (ivy amx)
+  :bind
+  ("C-x b" . switch-to-buffer)
+  :init
+  (counsel-mode 1))
 
 ;; projectile ---------------------
 (use-package projectile
-  :bind (("C-x p" . projectile-mode)
-	 :map projectile-mode-map
-	 ("C-x p n" . projectile-add-known-project)
-	 ("C-x p p" . projectile-switch-project))
-  :config (setq projectile-completion-system 'ivy)
-          (setq projectile-mode-line-function '(lambda () (format " [%s]" (projectile-project-name)))))
+  :bind
+  (("C-x p" . projectile-mode)
+    :map projectile-mode-map
+    ("C-x p n" . projectile-add-known-project)
+    ("C-x p p" . projectile-switch-project))
+  :custom
+  (projectile-completion-system 'ivy)
+  :config
+  (setq projectile-mode-line-function '(lambda () (format " [%s]" (projectile-project-name))))
+  :init
+  (projectile-mode 1))
 
 ;; magit --------------------------
 (use-package magit
-  :bind ("C-x g" . magit-status))
+  :bind
+  ("C-x g" . magit-status))
+
+;; yas-- --------------------------
+(use-package yasnippet
+  :init
+  (yas-global-mode 1))
 
 ;; org-mode -----------------------
 ;; define "C-c o" as a global prefix for org-related commands
@@ -99,7 +149,7 @@
 	  ("T" "Just a THOUGHT" entry
 	   (file ,(concat org-directory "/thoughts.org"))
 	   "* %?\n")
-	  ("q" "A QUOTE" entry
+	  ("Q" "A QUOTE" entry
 	   (file ,(concat org-directory "/quotes.org"))
 	   "* %?\n\n")
 	  ("b" "Add a BLOG post IDEA" entry
@@ -121,18 +171,30 @@
   (set-face-attribute 'org-list-dt nil :family "IBM Plex Mono")
   (set-face-attribute 'org-code 'default))
 
+(use-package mixed-pitch)
 ;; org-mode enhancements
-(defun custom-readability()
-  (variable-pitch-mode 'toggle)
-	    (setq line-spacing 3)
-	    (setq left-margin-width 2)
-	    (setq right-margin-width 2))
-(add-hook 'org-mode-hook #'custom-readability)
+(defun xstom/editing-mode()
+  (variable-pitch-mode 1)
+  (setq-local line-spacing 3)
+  (setq-local left-margin-width 3)
+  (setq-local right-margin-width 3))
+
+(defun xstom/org-mode()
+  (xstom/editing-mode)
+  (mixed-pitch-mode 1))
+
+(defun xstom/markdown-mode()
+  (xstom/editing-mode)
+  (setq-local line-spacing 4))
+
+(defun xstom/deft-mode()
+  (xstom/editing-mode))
+
+(add-hook 'text-mode-hook #'xstom/editing-mode)
+(add-hook 'org-mode-hook #'xstom/org-mode)
+
 (use-package org-indent
   :init (add-hook 'org-mode-hook #'org-indent-mode))
-(use-package mixed-pitch
-  :init (add-hook 'org-mode-hook #'mixed-pitch-mode))
-
 
 ;; org-ref ------------------------
 (use-package org-ref
@@ -143,6 +205,7 @@
 	org-ref-default-bibliography '("~/Dropbox/Notes/org/bibliography/references.bib")
 	org-ref-pdf-directory "~/Dropbox/Notes/org/bibliography/bibtex-pdfs/")
   (set-face-attribute 'org-ref-cite-face nil :weight 'semi-bold))
+
 ;; org-roam -----------------------
  (use-package org-roam
        :hook 
@@ -150,7 +213,7 @@
        :custom
        (org-roam-directory "~/Dropbox/Notes/org/knowledgebase")
        :config
-       (setq org-roam-capture-templates `(("d" "default" plain #'org-roam-capture--get-point "\n- refs :: \n- tags :: %?\n\n" :file-name "%<%Y%m%d%H%M%S>-${slug}" :head "#+TITLE: ${title}
+       (setq org-roam-capture-templates `(("d" "default" plain #'org-roam-capture--get-point "\n- refs :: \n- tags :: %?\n\n" :file-name "%<%Y%m%d%H%M%S>-${slug}" :head "#+title: ${title}\n#+created: \n
 " :unnarrowed t)))
        :bind (:map org-roam-mode-map
                (("C-c n l" . org-roam)
@@ -159,35 +222,41 @@
                :map org-mode-map
                (("C-c n i" . org-roam-insert))))
  
-;; If you installed via MELPA
+;; org-roam-bibtex --------------
 (use-package org-roam-bibtex
   :after org-roam
-  :hook (org-roam-mode . org-roam-bibtex-mode)
+;; :hook
+;; (org-roam-mode . org-roam-bibtex-mode)
   :bind (:map org-mode-map
          (("C-c n a" . orb-note-actions))))
-; deft --------------------------
+
+;; deft --------------------------
 (use-package deft
   :bind
   ("<f8>" . deft)
   ("C-c o f" . deft-find-file)
   :commands (deft)
-  :config (setq deft-directory org-directory 
-		deft-recursive t
-                deft-extensions '("org")
-		deft-default-extension "org"))
-
-(add-hook 'deft-mode-hook #'custom-readability)
+  :config
+  (setq deft-directory org-directory 
+	deft-recursive t
+        deft-auto-save-interval -1.0
+	deft-extensions '("org")
+	deft-default-extension "org")
+  :init 
+  (add-hook 'deft-mode-hook #'xstom/deft-mode))
 
 ;; markdown-mode -----------------
 (use-package markdown-mode
   :init
-  (add-hook 'markdown-mode-hook 'yas-minor-mode)
-  (add-hook 'markdown-mode-hook #'custom-readability))
+  (add-hook 'markdown-mode-hook #'xstom/markdown-mode)
+  (add-hook 'markdown-mode-hook 'wc-mode))
 
 ;; evil-snipe --------------------
 (use-package evil-snipe
-  :config (setq evil-snipe-scope 'buffer)
-  :init (evil-snipe-mode 1))
+  :custom
+  (evil-snipe-scope 'buffer)
+  :init
+  (evil-snipe-mode 1))
 ;; evil-snipe causes problems with magit buffers; override
 ;; (add-hook 'magit-mode-hook 'turn-off-evil-snipe-override-mode)
 
@@ -206,23 +275,24 @@
 
 ;; start modes
 (evil-mode 1)
-(recentf-mode 1)
-(ivy-mode 1)
-(projectile-mode 1)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(amx-backend (quote auto))
  '(custom-safe-themes
    (quote
     ("99ea831ca79a916f1bd789de366b639d09811501e8c092c85b2cb7d697777f93" "9b01a258b57067426cc3c8155330b0381ae0d8dd41d5345b5eddac69f40d409b" "bf387180109d222aee6bb089db48ed38403a1e330c9ec69fe1f52460a8936b66" "3577ee091e1d318c49889574a31175970472f6f182a9789f1a3e9e4513641d86" "a92e9da0fab90cbec4af4a2035602208cebf3d071ea547157b2bfc5d9bd4d48d" "3d3807f1070bb91a68d6638a708ee09e63c0825ad21809c87138e676a60bda5d" "bc836bf29eab22d7e5b4c142d201bcce351806b7c1f94955ccafab8ce5b20208" default)))
+ '(evil-snipe-scope (quote buffer))
  '(fancy-splash-image "/home/monk/.local/share/icons/emacs.svg")
  '(olivetti-body-width 120 t)
  '(org-roam-directory "~/Dropbox/Notes/org/knowledgebase")
  '(package-selected-packages
    (quote
-    (ivy-bibtex bibtex-completion org-roam-server org-roam-bibtex company-org-roam org-ref org-superstar yatemplate yasnippet-snippets yasnippet olivetti org-evil evil-org use-package projectile org-roam mixed-pitch markdown-mode magit ivy evil-snipe doom-themes delight deft crux))))
+    (wc-mode yasnippet-lean hl-todo zeno-theme winum amx ivy-bibtex bibtex-completion org-roam-server org-roam-bibtex company-org-roam org-ref org-superstar yatemplate yasnippet-snippets yasnippet olivetti org-evil evil-org use-package projectile org-roam mixed-pitch markdown-mode magit ivy evil-snipe doom-themes delight deft crux)))
+ '(projectile-completion-system (quote ivy))
+ '(recentf-max-saved-items 100))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
